99550f3008dd1fb5988513b78f1e2fb24a061fbe
Alex Bilson <acbilson@gmail.com>
2019-09-27 08:40:59 -0500

    adds logging to server health check

--- server-health-check.ps1
+++ server-health-check.ps1
@@ -5,6 +5,9 @@ curl -Method GET -Uri http://localhost:5000/api/Folder/GetFileSchema -UseBasicPa
 # LedgerAPI
 curl -Method GET -Uri "http://localhost:5000/api/Ledger/Read?fileName=checking&index=0&count=5" -UseBasicParsing | ConvertFrom-Json | Format-List
 
+# LoggingAPI
+curl -Method POST -Uri http://localhost:5000/api/Logging/Log -Body "{ Level: 1, Message:'This is a server test', Timestamp:'', Context:'Powershell'}" -UseBasicParsing -Headers @{ "Content-Type"="application/json" }
+
 # BudgetAPI
 $response = curl -Method GET -Uri "http://localhost:5000/api/Budget/Read?start=2018-04-01&end=2018-04-30" -UseBasicParsing | ConvertFrom-Json
 $response | select -First 5
