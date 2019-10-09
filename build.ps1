95250cb384307351610a6773ba6e3e040dcd5b9d
Alex Bilson <acbilson@gmail.com>
2019-10-09 15:37:49 -0500

    removes a couple extra files

--- build.ps1
+++
@@ -1,5 +0,0 @@
-###########################
-# Build instructions for WSL using Docker with Ubuntu 18.04
-###########################
-
-dotnet publish -c Release --no-build --self-contained -o build
