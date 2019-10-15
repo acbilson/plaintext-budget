9def4a756c45d1fab7adff1d8d361c15ef11cbde
Alex Bilson <acbilson@gmail.com>
2019-10-15 14:40:39 -0500

    updates build script with compose commands

--- build.ps1
+++ build.ps1
@@ -1,26 +1,21 @@
 param(
   [Parameter(Mandatory)]
   [ValidateNotNullOrEmpty()]
-  [ValidateSet("Build", "Run", "Bash")]
+  [ValidateSet("Start", "Stop")]
   [String]$Action
 )
 
 switch ($Action) {
 
-  "Build" {
+  "Start" {
 
-    # creates a complete Docker image for the PTB app
-    docker build --file='docker/Dockerfile' --tag='ptb-test' .
-  }
-  "Run" {
-
-    # creates container and launches server on localhost:4000
-    docker run --rm -p 4000:80 ptb-test:latest dotnet PTB.Web.dll
-  }
-  "Bash" {
-
-    # enters bash on docker container to review
-    docker exec -it ptb-test /bin/bash
+    # runs both web and server in detached mode
+    docker-compose up -d
   }
 
+  "Stop" {
+
+    docker-compose down
+
+  }
 }
